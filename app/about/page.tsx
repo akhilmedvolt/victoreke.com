7413abeafa47640de67fe2328774f5479e735701
akhilmedvolt <reekithak@gmail.com>
2024-08-25 20:21:35 +0530

    update: resume

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -72,7 +72,7 @@ export default async function About() {
                     <div className="flex flex-col text-center gap-y-4">
                       <div className="flex items-center gap-x-3">
                         <RefLink
-                          href="https://drive.google.com/file/d/1u4VnvZtU69U9qSLkEl2o49_8nhqmD_ly/view?usp=sharing"
+                          href="https://bit.ly/akhil-sanker-resume"
                           className="flex items-center justify-center text-center gap-x-2 basis-[90%] dark:bg-primary-bg bg-zinc-100 border border-transparent dark:hover:border-zinc-700 hover:border-zinc-200 rounded-md py-2 text-lg font-incognito font-semibold"
                         >
                           View Résumé <BiLinkExternal className="text-base" />
